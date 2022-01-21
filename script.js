5995f67f474a0543a3898f4cd8224769c18ec86d
YogeshParihar1498 <42130173+YogeshParihar1498@users.noreply.github.com>
2022-01-20 21:00:06 -0500

    third

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Software Developer", "Web Devloper" , "Student"],
+        strings: [ "Student","Software Developer", "Web Developer" ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Software Developer", "Web Devloper" , "Student"],
+        strings: [ "Student","Software Developer", "Web Developer" ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
