59cc1f46b3b4555407c6f737af96a243c4eed832
Yogesh Parihar <yogeshparihar1498@gmail.com>
2024-08-21 21:19:15 -0400

    New changes

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Student","Software Developer", "Web Developer" ],
+        strings: [ "Software Engineer", "Automation Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Student","Software Developer", "Web Developer" ],
+        strings: [ "Automation Developer","Software Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
